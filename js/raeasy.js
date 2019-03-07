583498f100b65d333b61ea4bba1e745b3d735e20
Rodney Somoire <rayr30667@gmail.com>
2019-03-07 15:33:52 -0500

    almost done with this thing

--- js/raeasy.js
+++ js/raeasy.js
@@ -1,1 +1,24 @@
-$("#myCarousel").carousel();
+$(document).ready(function(){
+
+  $("#myCarousel").carousel();
+
+  $(".item1").click(function(){
+    $("#myCarousel").carousel(0);
+  });
+  $(".item2").click(function(){
+    $("#myCarousel").carousel(1);
+  });
+  $(".item3").click(function(){
+    $("#myCarousel").carousel(2);
+  });
+  $(".item4").click(function(){
+    $("#myCarousel").carousel(3);
+  });
+
+  $(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#myCarousel").carousel("next");
+  });
+});
