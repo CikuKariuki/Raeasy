99a1d1ffbbf3b383c1e1e211e6489fe9ec4f8bc0
Ciku Kariuki <wanjikukariuki03@gmail.com>
2019-03-08 05:09:53 +0300

    added styling

--- js/raeasy.js
+++ js/raeasy.js
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-$("document").ready( function() {
- 
+$(document).ready(function() {
+
     $("button.searchButton").hover(function(){
         $(".searchTerm").fadeIn(3000);
       });
-   
-=======
+});
 $(document).ready(function(){
   // Activate Carousel
   $("#myCarousel").carousel();
-
+});
   // Enable Carousel Indicators
-=======
+
 $(document).ready(function(){
 
   $("#myCarousel").carousel();
 
->>>>>>> origin/feature/Rodney
+
   $(".item1").click(function(){
     $("#myCarousel").carousel(0);
   });
@@ -31,18 +28,27 @@ $(document).ready(function(){
     $("#myCarousel").carousel(3);
   });
 
-<<<<<<< HEAD
-  // Enable Carousel Controls
-=======
->>>>>>> origin/feature/Rodney
+
   $(".left").click(function(){
     $("#myCarousel").carousel("prev");
   });
   $(".right").click(function(){
     $("#myCarousel").carousel("next");
   });
-<<<<<<< HEAD
->>>>>>> origin/feature/Rodney
-=======
->>>>>>> origin/feature/Rodney
+
+});
+$(document).ready(function() {
+
+  $("button#login").click(function() {
+    var userName=$("input#uname").val();
+    var passWord=$("input#psw").val();
+    // $("#1show").text(player1);
+    // $("#2show").text(player2);
+    $(".p1").hide();
+    $(".p2").show();
+// })
+//   $("p#sign").click(function() {
+//     (".signuppage").show();
+});
+
 });
